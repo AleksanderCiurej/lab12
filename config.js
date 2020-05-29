b24a0c04cb1eb73fc2b823f1a531a56fe8a93662
Aleksander <aleksander.ciurej@gmail.com>
2020-05-29 15:20:28 +0200

    fix

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.MONGODB_URI || 'mongodb://tai:taitai1@ds147180.mlab.com:47180/tai',
-  JwtSecret: process.env.JWT_SECRET || 'secret'
-};
+  databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://aleksander:mongopasswd@tai-axoy7.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  JwtSecret: process.env.JWT_SERCRET || 'secret'
 
-export default config;
+}
+export default config
